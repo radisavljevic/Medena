5946bded79e14f3dec16c63cef864b70400e4a63
radisavljevic <ropeta011@gmail.com>
2018-05-07 14:18:28 +0200

    commit1

---
+++ Medena/DB/ConnectionClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlServerCe;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace Medena.DB
+{
+    class ConnectionClass
+    {
+        //SqlCeCommand cmd;
+
+        public static void dajArtikle()
+        {
+            try
+            {
+                using (SqlCeConnection cn = new SqlCeConnection(ConfigurationManager.ConnectionStrings["ConnectionString1"].ToString()))
+                {
+                    SqlCeCommand cmd = new SqlCeCommand("SELECT naziv FROM Artikli", cn);
+                    cn.Open();
+                    SqlCeDataReader reader1 = cmd.ExecuteReader();
+
+                    while (reader1.Read())
+                    {
+                        MessageBox.Show("Result :" + reader1[0]);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error :" + ex.ToString());
+
+            }
+
+        }
+
+
+
+
+    }
+
+}
+
+
--- Medena/Form1.cs
+++ Medena/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Medena.DB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,8 @@ namespace Medena
         public Form1()
         {
             InitializeComponent();
+            ConnectionClass.dajArtikle();
         }
+       
     }
 }
